b522be44077e3b7dd5341e96c79a526f809936c1
Konrad Zdeb <konrad.zdeb@me.com>
2018-04-15 20:34:01 +0100

    Added scratchpad with non-production code

---
+++ scratchpad.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+
+"""
+The following file contains code snippet not pertinent to the actual
+task.
+"""
+
+# Read breast_cancer_data to pandas (in case of further analysis)
